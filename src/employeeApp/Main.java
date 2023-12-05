2a2116d826e8065b3f03dca81bb49e078741b259
melihozcelik17 <melih.ozcelik17@gmail.com>
2023-12-05 23:21:53 +0300

    s13 tamamlandı.

--- src/employeeApp/Main.java
+++ src/employeeApp/Main.java
@@ -1,9 +1,12 @@
 package employeeApp;
 
-/**
- * Uygulamanın amacı Java'da nesne tabanlı programlamanın temellerinin oluşturulmasıdır.
- *
- */
+import employeeApp.enums.Plan;
+import employeeApp.model.Company;
+import employeeApp.model.Employee;
+import employeeApp.model.Healthplan;
+
+import java.util.Arrays;
+
 public class Main
 {
 
@@ -12,7 +15,41 @@ public class Main
      */
     private static void workWithData ()
     {
+        Healthplan plan1 = new Healthplan(1, "A Sigorta", Plan.ADVANCE);
+        Healthplan plan2 = new Healthplan(2, "B Sigorta", Plan.MEDIUM);
 
+        System.out.println(plan1);
+        System.out.println(plan2);
+
+        String[] healtplans = new String[3];
+        healtplans[0] = plan1.getName();
+        healtplans[1] = plan2.getName();
+
+        //["A Sigorta", "B Sigorta"]
+        Employee employee = new Employee(1, "Ali Veli",
+                "aliveli.com", "1234", healtplans);
+
+        employee.addHealthplan(0, "C Sigorta");
+        employee.addHealthplan(2, "A Sigorta");
+        employee.addHealthplan(2, "C Sigorta");
+        employee.addHealthplan(3, "T Sigorta");
+        System.out.println(Arrays.toString(employee.getHealthPlans()));
+
+        System.out.println("**********************************");
+        Company company = new Company(1, "Workintech",
+                1000000000, new String[]{"Dogancan Kınık", "Omer Fatih Tanriverdi", null});
+
+        company.addEmployee(0, "Ali Veli");
+        company.addEmployee(2, "Mehmet Ali");
+        company.addEmployee(-1, "Mehmet Ali");
+        company.addEmployee(5, "Mehmet Ali");
+
+        System.out.println(Arrays.toString(company.getDeveloperNames()));
+        System.out.println(company);
+
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        //double num = 1000000000;
     }
 
     /**
---
+++ src/employeeApp/model/Employee.java
@@ -0,0 +1,109 @@
+package employeeApp.model;
+
+import java.util.Arrays;
+
+//POJO(Plain Old Java Object)
+public class Employee {
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String[] healthPlans;
+
+    public Employee(int id, String fullName){
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public Employee(int id, String fullName, String email, String password, String[] healthPlans) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.healthPlans = healthPlans;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String[] getHealthPlans() {
+        return healthPlans;
+    }
+
+    public void setHealthPlans(String[] healthPlans) {
+        this.healthPlans = healthPlans;
+    }
+
+    //healthplans ["A Sigorta", null, null, null, null]
+    //flag(boolean)
+    public void addHealthplan(int index, String name){
+        if(index < 0){
+            System.out.println("Index değeri 0'dan küçük olamaz.");
+        } else {
+            try {
+                boolean isExist = false;
+                for (String healthplan : healthPlans) {
+                    if (healthplan != null && healthplan.equals(name)) {
+                        isExist = true;
+                        System.out.println(name + " dizi içerisinde mevcut.");
+                        break;
+                    }
+                }
+
+                if (!isExist) {
+                    if (healthPlans[index] == null) {
+                        healthPlans[index] = name;
+                    } else {
+                        System.out.println("İlgili index dolu");
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException ex){
+                System.out.println("Invalid index: " + index);
+            } catch (NullPointerException npe){
+                System.out.println("Null Pointer Exception: " + npe.getMessage());
+            } catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", healthPlans=" + Arrays.toString(healthPlans) +
+                '}';
+    }
+}
